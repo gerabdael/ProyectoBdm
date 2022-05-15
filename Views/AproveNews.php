bf68e240aeeaaffdd789a6cd430af865e407e3df
Emiliano Leal <72473356+EmilianoLeal@users.noreply.github.com>
2022-05-15 16:36:43 -0500

    AproveNews

---
+++ Views/AproveNews.php
@@ -0,0 +1,74 @@
+<?php include('./HeaderFinal.php')?>
+
+
+<div class="NewStuff">
+
+<div class="NuevaNoticia">
+
+
+<table class="table table-striped table-dark">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Titulo noticia</th>
+      <th scope="col">Descripcion</th>
+      <th scope="col">Comentario</th>
+      <th scope="col">Publicar</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Perro policia</td>
+      <td>Un perro que se hizo policia</td>
+      <td>
+      <div class="d-flex flex-row align-items-start"><textarea class="form-control ml-1 shadow-none textarea"></textarea></div>
+      </td>
+
+      <td>
+      <button type="button" class="btn btn-success">Aprovada</button>
+      <button type="button" class="btn btn-danger">Negada</button>
+      </td>
+    </tr>
+
+    <tr>
+      <th scope="row">2</th>
+      <td>Perro bombero</td>
+      <td>Un perro que se hizo bombero</td>
+      <td>
+      <div class="d-flex flex-row align-items-start"><textarea class="form-control ml-1 shadow-none textarea"></textarea></div>
+      </td>
+
+      <td>
+      <button type="button" class="btn btn-success">Aprovada</button>
+      <button type="button" class="btn btn-danger">Negada</button>
+      </td>
+    </tr>
+
+    <tr>
+      <th scope="row">3</th>
+      <td>Perro astronauta</td>
+      <td>Un perro que se hizo astronauta</td>
+      <td>
+      <div class="d-flex flex-row align-items-start"><textarea class="form-control ml-1 shadow-none textarea"></textarea></div>
+      </td>
+
+      <td>
+      <button type="button" class="btn btn-success">Aprovada</button>
+      <button type="button" class="btn btn-danger">Negada</button>
+      </td>
+    </tr>
+
+  
+  </tbody>
+</table>
+       
+        
+
+
+</div>
+
+</div>
+
+
+<?php include('./footer.php')?>
